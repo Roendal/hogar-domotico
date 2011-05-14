0ac38cb33535c7441c707e4ac12b774462294bed
Eduardo Casanova <ecasanovac@gmail.com>
2011-05-14 17:02:48 +0200

    Alarm sound with Java beep support. Done some clean up.

--- src/main/java/jadex/bdi/examples/cleanerworld_classic/ambrosio/ActivateAlarmPlan.java
+++ src/main/java/jadex/bdi/examples/cleanerworld_classic/ambrosio/ActivateAlarmPlan.java
@@ -2,8 +2,15 @@ package jadex.bdi.examples.cleanerworld_classic.ambrosio;
 
 import jadex.bdi.runtime.Plan;
 
+import java.awt.Toolkit;
+
 public class ActivateAlarmPlan extends Plan {
 	// -------- constructors --------
+
+	private int alarm_time = 120;
+	private int alarm_times = 4;
+	private int alarm_wave_wait = 250;
+	private int alarm_waves = 2;
 
 	/**
 	 * Create a new plan.
@@ -18,6 +25,15 @@ public class ActivateAlarmPlan extends Plan {
 	 */
 	public void body() {
 		System.out.println("RINGGGGGGGGGGGG!");
+		for (int j = 0; j < alarm_waves; j++) {
+			for (int i = 0; i < alarm_times; i++) {
+				Toolkit.getDefaultToolkit().beep();
+				waitFor(alarm_time);
+			}
+			waitFor(alarm_wave_wait);
+		}
+
+		getBeliefbase().getBelief("alarmcondition").setFact(false);
 	}
 
 }
